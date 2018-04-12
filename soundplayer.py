010af20b2f992a2e2d904b1ae949760d26ff156d
Eddie Lee <edl56@cornell.edu>
2018-04-12 14:15:52 -0400

    Updates to soundplayer to convert Eddie_Grid_Model_2

--- soundplayer.py
+++ soundplayer.py
@@ -1,15 +1,15 @@
+# =============================================================================================== #
+# Module for writing mocap files to audio. 
+# Author : Saerom Choi
+# =============================================================================================== #
 import os, wave, struct, math, sys, numpy as np, matplotlib.pyplot as plt
-from pipeline import extract_motionbuilder_model3_3
+from pipeline import extract_motionbuilder_model3_3,extract_motionbuilder_Eddie_Grid_Model_2
 from scipy.signal import fftconvolve
 from scipy import interpolate as itp
 from scipy.special import expit
-from openal import *
 import numpy.fft as fft
 import pyaudio
 
-
-
-SOUND_DIR = "soundfiles_leftright"
 
 
 def load_sound(filename):
@@ -49,7 +49,8 @@ def chunks(l, n):
 
 class SoundPlayer():
 	def __init__(self, directory, model_name, direction, reverse_time):
-		self.models = {'3_3': extract_motionbuilder_model3_3 }
+		self.models = {'3_3': extract_motionbuilder_model3_3,
+                               'Eddie_Grid_Model_2': extract_motionbuilder_Eddie_Grid_Model_2 }
 		self.model = self.models[model_name]
 		self.function_dict = {'exp': (np.exp, 0.75), 'log': (np.log,0.5), 'sig': (expit,0.5), '': (lambda x : x, 32.0)}
 		self.direction = direction
@@ -139,6 +140,17 @@ class SoundPlayer():
 
 
 	def save_sound(self, filename, key, duration=None):
+                """Convert motion into WAV audio file.
+                Parameters
+                ----------
+                filename : str
+                key : str
+                duration : float,None
+
+                Returns
+                -------
+                None
+                """
 		data_amp = [np.linalg.norm(x) for x in self.data]
 		smooth_data = fftconvolve(data_amp,np.ones(12)/12.0)
 		smooth_data = fftconvolve(smooth_data[::-1],np.ones(12)/12.0)[::-1]
@@ -162,7 +174,7 @@ class SoundPlayer():
 		wav_samplerate = 44100.0
 
 		# opening wav file
-		wavef = wave.open(os.path.join(SOUND_DIR,'_'.join([filename,key,self.direction])+'.wav'),'w')
+		wavef = wave.open(os.path.join(self.directory,'_'.join([filename,key,self.direction])+'.wav'),'w')
 		wavef.setnchannels(2)
 		wavef.setsampwidth(2) 
 		wavef.setframerate(wav_samplerate)
@@ -208,6 +220,17 @@ class SoundPlayer():
 
 
 if __name__ == '__main__':
-	sp = SoundPlayer(SOUND_DIR,"3_3","Right",False)
-	sp.save_sound_leftright("interpolate_leftright","exp")
+    """Example call:
+    python exp /Users/eddie/Downloads Eddie_Grid_Model_2 Right False
+    """
+    mapType=sys.argv[1]
+    dr=sys.argv[2]
+    motionFileName=sys.argv[3]
+    hand=sys.argv[4]
+    reverseTime=sys.argv[5]
+    assert mapType in ('exp','log','sig')
+    assert os.path.isdir(dr)
+
+    sp = SoundPlayer(dr,motionFileName,hand,reverseTime)
+    sp.save_sound_leftright("motion_to_audio",mapType)
 
